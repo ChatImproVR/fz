438358026480b644fb53fb4228fece064612fd35
Masterchef365 <duncan.freeman1@gmail.com>
2023-02-10 00:36:28 -0800

    It shows a ship!

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
-use cimvr_engine_interface::{make_app_state, prelude::*, println};
+use cimvr_common::{
+    render::{Primitive, Render, RenderData, RenderHandle},
+    Transform,
+};
+use cimvr_engine_interface::{make_app_state, pkg_namespace, prelude::*, println};
+
+use crate::obj::obj_lines_to_mesh;
+
+mod obj;
 
 // All state associated with client-side behaviour
 struct ClientState;
 
+pub const SHIP_RDR: RenderHandle = RenderHandle::new(pkg_namespace!("Ship"));
+
 impl UserState for ClientState {
     // Implement a constructor
-    fn new(_io: &mut EngineIo, _sched: &mut EngineSchedule<Self>) -> Self {
-        println!("Hello, client!");
+    fn new(io: &mut EngineIo, _sched: &mut EngineSchedule<Self>) -> Self {
+        //let mesh = obj_lines_to_mesh(include_str!("assets/ship.obj"));
+        let mesh = obj_lines_to_mesh(include_str!("assets/ship.obj"));
+        io.send(&RenderData { mesh, id: SHIP_RDR });
 
         // NOTE: We are using the println defined by cimvr_engine_interface here, NOT the standard library!
         cimvr_engine_interface::println!("This prints");
@@ -21,7 +33,12 @@ struct ServerState;
 
 impl UserState for ServerState {
     // Implement a constructor
-    fn new(_io: &mut EngineIo, _sched: &mut EngineSchedule<Self>) -> Self {
+    fn new(io: &mut EngineIo, _sched: &mut EngineSchedule<Self>) -> Self {
+        let ent = io.create_entity();
+        io.add_component(ent, &Transform::identity());
+        io.add_component(ent, &Render::new(SHIP_RDR).primitive(Primitive::Lines));
+        io.add_component(ent, &Synchronized);
+
         println!("Hello, server!");
         Self
     }
@@ -30,10 +47,3 @@ impl UserState for ServerState {
 // Defines entry points for the engine to hook into.
 // Calls new() for the appropriate state.
 make_app_state!(ClientState, ServerState);
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn im_a_test() {}
-}
---
+++ src/obj.rs
@@ -0,0 +1,55 @@
+use cimvr_common::render::{Mesh, Vertex};
+use cimvr_engine_interface::{dbg, prelude::*};
+use std::{io::Read, str::FromStr};
+
+/// Read OBJ lines into the mesh
+pub fn obj_lines_to_mesh(obj: &str) -> Mesh {
+    let mut m = Mesh::new();
+
+    for line in obj.lines() {
+        // Split the line by whitespace
+        let mut line = line.split_whitespace();
+
+        // Break the first bit off
+        let (first, mut rest) = (line.next(), line);
+
+        // Which kind of line is it?
+        match first {
+            Some("v") => {
+                // Treat the line as two arrays of 3 elements
+                let mut parts = [[0.; 3], [1.; 3]];
+
+                for part in &mut parts {
+                    // Get strings from the rest of the line
+                    // The by_ref() here allows us to keep eating the line on the next loop
+                    for dim in part {
+                        let Some(text) = rest.next() else { break };
+                        *dim = text.parse().expect("Invalid float");
+                    }
+                }
+
+                // Split the parts back up
+                let [pos, uvw] = parts;
+
+                // Assemble the vertex
+                m.vertices.push(Vertex { pos, uvw });
+            }
+            Some("l") => {
+                // Do the same for indices
+                let mut indices = [0; 2];
+                for dim in &mut indices {
+                    let Some(text) = rest.next() else { break };
+                    *dim = text.parse().expect("Invalid index");
+
+                    // OBJ files are one-indexed
+                    *dim -= 1;
+                }
+                m.indices.extend(indices);
+            }
+            // Ignore the rest
+            _ => (),
+        }
+    }
+
+    m
+}
